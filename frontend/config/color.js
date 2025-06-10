67b613578f6b4b8ac279cb84164f1d49740a6d23
Maria Ferrandiz <mariaferrandizdiaz2003@gmail.com>
2025-06-11 00:22:40 +0100

    Añadido fichero de colores y logo de al herramienta

---
+++ frontend/config/color.js
@@ -0,0 +1,85 @@
+// config/colors.js
+const primary = '#5C068C';   // Violeta oscuro
+const secondary = '#CAA3DF'; // Violeta claro
+const tertiary = '#A55235';  // Naranja quemado
+const black = '#1D1D1D';  // Negro grisáceo
+const white = '#FFFFFF';  // Blanco
+const gray = '#c9c9c9';   // Gris claro
+const darkGreen = '#25594A'; // Verde oscuro para botones
+const red = '#eabcc6'; // Rojo error
+const lightGreen = '#d2edc7'; // Verde éxito
+const lightGray = '#f5f5f5'; // Gris de fondo
+
+export default {
+  // Generales
+  colorPrimario: primary,
+  colorSecundario: secondary,
+  colorTerciario: tertiary,
+  colorTextoPrimario: black,
+  colorTextoSecundario: white,
+  colorTextoSecundarioHover: gray,
+  colorError: red,
+  colorAcierto: lightGreen,
+
+  // Main page
+  colorFondoLoginBox: white,
+  colorFondoLoginPageFrom: secondary,
+  colorFondoLoginPageTo: '#D0ECE7',
+  colorBotonSubmit: darkGreen,
+  colorTextoBotonSubmit: white,
+  colorBotonSubmitHover: '#1E4A3E',
+  colorBotonSubmitBorde: black,
+  colorBotonGoogle: white,
+  colortBotonTextoGoogle: '#3C4043',
+  colorBotonGoogleHover: lightGray,
+  colorBotonGoogleBorde: gray,
+
+  // Theory page
+  colorBounceLoading1: secondary,
+  colorBounceLoading2: primary,
+  colorBounceLoading3: tertiary,
+  colorBounceLoading4: '#25594A',
+  colorToggleOn: primary,
+  colorToggleOff: '#6E6E6E',
+  colorToggleRing: primary,
+  colorBotonToggle: white,
+  colorPrincipalProfilePage: secondary,
+
+  // Header
+  colorFondoHeader: white,
+
+  // Footer
+  colotBotonDonativo: primary,
+  colorIconoRedSocial: white,
+  colorIconoRedSocialHover: gray,
+
+  // UserInfo
+  colorIcono: white,
+  colorFocusMenu: gray,
+
+  // PersonCard
+  colorFondoCard: white,
+
+  // ChangeDataAndPasswordForm
+  colorBotonGuardar: primary,
+  colorFocusFormularioChange: primary,
+
+  // Services
+  colorFondoEncabezadosTabla: lightGray,
+  colorBordeIconoServicios: '#6E6E6E',
+  colorBotonEnviarCorreo: darkGreen,
+  colorTextoBotonEnviarCorreo: white,
+  colorBotonEnviarCorreoHover: '#1E4A3E',
+  colorBordeBotonEnviarCorreo: black,
+  colorCancelarServicio: red,
+  colorTextoCancelarServicio: red,
+  colorCancelarServicioHover: red,
+  colorCancelarServicioHoverTexto: white,
+  colorContratarServicio: primary,
+  colorTextoContratarServicio: '#917313',
+  colorContratarServicioHover: primary,
+  colorContratarServicioHoverTexto: black,
+
+  // DownloadButton
+  colorBotonDescargar: secondary,
+};
